9c888eab3dc98d44f164d9874048dad0c52769fb
stuart kan <stukannn@gmail.com>
2024-12-06 21:23:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-'''
-
-
-function updateDateTime() {
-    const now = new Date();
-    const datetimeElement = document.getElementById('datetime');
-    datetimeElement.textContent = `Local Date and Time: ${now.toLocaleString()}`;
-}
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    updateDateTime();
-    setInterval(updateDateTime, 1000); // Update every second
-});
-
-'''
-
 function updateDateTime() {
     const now = new Date();
     const localDatetimeElement = document.getElementById('local-datetime');
